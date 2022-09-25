81dfaff93ceaca1a8192b066f36047b9c5721cd8
BotWhan <68204459+BotWhan@users.noreply.github.com>
2022-09-25 00:44:57 -0400

    Update new_game.js

--- com.pa.bot.human-value-index/ui/mods/com.pa.bot.human-value-index/new_game.js
+++ com.pa.bot.human-value-index/ui/mods/com.pa.bot.human-value-index/new_game.js
@@ -1,3 +1,5 @@
+//http://nuttygroup.org:27395/?case=0&uberid=6089886685367004270
+
 const HVIURL = 'http://nuttygroup.org:27395/?';
 function getWordRating(rating) {
   if (rating <= 3) {
@@ -11,7 +13,7 @@ function getWordRating(rating) {
   }
   return "Very Good";
 }
-
+//api.net.ubernet('/GameClient/UserId?' + $.param({TitleDisplayName: "(name)" }), 'GET', 'text')
 var updateindex = ko.observable(0);
 
 model.determineAuth = function (){
@@ -23,6 +25,7 @@ var auth = ko.observable();
 model.determineAuth();
 
 model.update = function () {
+
   $.get(HVIURL, function (update) {
     if(update != updateindex()){
       updateindex(update);
@@ -33,7 +36,9 @@ setInterval(function(){model.update();}, 10000);
 model.humanRatings = function (slot) {
   var value = ko.observable();
   function get() {
-    $.getJSON(HVIURL + "case=0&uberid=" + slot.playerId() + "&name=" + slot.playerName(), function (profile) {
+    api.net.ubernet('/GameClient/UserId?' + $.param({TitleDisplayName: slot.playerName() }), 'GET', 'text').then(function (ubernetID){
+      gotPlayerId =  JSON.parse(ubernetID).UberId;
+    $.getJSON(HVIURL + "case=0&uberid=" + gotPlayerId + "&name=" + slot.playerName(), function (profile) {
       if (profile.name != "not registered") {
         value(((slot.playerName() == profile.name) ? "" : "(" + profile.name + ") | ") + profile.rating + ", " + getWordRating(profile.rating));
       }
@@ -44,21 +49,29 @@ model.humanRatings = function (slot) {
         value("");
       }
     })
+  })
   }
+  
   get();
   updateindex.subscribe(function(){
       get();
   });
   return { value: value};
 }
+function logNamesAndIds(slot){
+  console.log(slot.playerName() +"'s uberId = " + slot.playerId())
+  console.log(slot.playerName() == slot.playerId())  
+}
 
 $('div.slot-player-text.truncate').after(
-  '<!-- ko if: (!slot.ai() &&(slot.playerId() > 33) && slot.isPlayer() && (typeof slot.playerId() != "undefined"))-->\
-     <!-- ko with: model.humanRatings(slot) -->\
-      <small data-bind="click: function(){editBox(slot)}"><small data-bind="text:value"></small></small>\
-      <!-- /ko --><!-- /ko -->')
+  '<!-- ko if: (!slot.ai() && (typeof slot.playerId() != "undefined"))-->\
+  <!-- ko with: model.humanRatings(slot) -->\
+  <small data-bind="click: function(){editBox(slot)}"><small data-bind="text:value"></small></small>\
+  <!-- /ko --><!-- /ko -->')
 
 function editBox(slot) {
+  api.net.ubernet('/GameClient/UserId?' + $.param({TitleDisplayName: slot.playerName() }), 'GET', 'text').then(function (ubernetID){
+    gotPlayerId =  JSON.parse(ubernetID).UberId;
   if(!auth()){
     return
   }
@@ -69,14 +82,25 @@ function editBox(slot) {
     'console.log(document.getElementById(\'newRating\').value); ' +
     'document.getElementById(\'newRating\').value.length == 0 ? ' +
     'document.getElementById(\'newRating\').value = \'\' : ' +
-    'toServer(\'1\',\'' + slot.playerId() + '\',document.getElementById(\'newRating\').value,\'' + slot.playerName() + '\',\'' + model.uberId() + '\');">' +
+    'toServer(\'1\',\'' + gotPlayerId + '\',document.getElementById(\'newRating\').value,\'' + slot.playerName() + '\',\'' + model.uberId() + '\');">' +
     '<small>Submit</small></button>' +
     '<br>' +
     '<button type="button" id="closeButton" onclick="document.getElementById(\'editPlayerBox\').remove()"><small>X</small></button>';
+})
 }
 
-
 function toServer(caseNum, id, rating, name) {
+  console.log("&authentication=" + model.uberId());
   $.get(HVIURL + "case=" + caseNum + "&uberid=" + id + "&rating=" + rating + "&name=" + name + "&authentication=" + model.uberId());
   setTimeout(function(){model.update()}, 2000);
+}
+model.getNameAndId = function () {
+  model.armies().forEach(function eachArmy(index) {
+    index.slots().forEach(function eachSlot(index) {
+      if (index.playerName() != null || index.playerName() != "") {
+        console.log(index.playerName() + " " + index.playerId());
+      }
+    }
+    );
+  });
 }
